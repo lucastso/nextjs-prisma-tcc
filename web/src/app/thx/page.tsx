153869c1947c1937e3f6380f01aae55af155df3b
Lucas Tassi Souza <lucastassisouza@gmail.com>
2023-06-20 20:01:38 -0300

    feat: thx page

--- web/src/app/thx/page.tsx
+++ web/src/app/thx/page.tsx
@@ -1,3 +1,20 @@
+import { Check } from 'lucide-react'
+import Link from 'next/link'
+
 export default function Thx() {
-  return <div>aaa</div>
+  return (
+    <div className="mx-auto mb-auto mt-8 overflow-x-hidden xs:w-full xs:px-6 lg:w-4/6 lg:px-0">
+      <span className="mb-4 flex items-center gap-1 text-xl font-semibold">
+        Compra concluída! <Check size={16} />
+      </span>
+      <span>
+        Obrigado por comprar, logo você terá seus produtos em sua casa. Você
+        pode{' '}
+        <Link href="/" className="font-semibold underline">
+          voltar para o início
+        </Link>{' '}
+        agora.
+      </span>
+    </div>
+  )
 }
