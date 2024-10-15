183b6d4e450b224b170f060849ee681647db879f
Lucas Tassi Souza <lucastassisouza@gmail.com>
2024-10-15 20:04:41 -0300

    fix: routes

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
-  publicRoutes: ['/', '/terms', '/products/(.*)'],
+  publicRoutes: ['/', '/terms', '/products/(.*)', '/products'],
 })
 
 export const config = {
